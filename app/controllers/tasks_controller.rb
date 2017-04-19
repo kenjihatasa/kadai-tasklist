7d20564640b5c83f1b2a711e00ad5a17306d3bcc
kenjihatasa <kenjinke0723@gmail.com>
2017-04-19 05:08:49 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
 
     if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'task は更新されませんでした'
       render :edit
